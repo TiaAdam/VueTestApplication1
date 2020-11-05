d4a847c12026f267484a47f0d7ba6b352c855f62
TiaAdam <44812614+TiaAdam@users.noreply.github.com>
2020-11-05 01:34:37 +0000

    Adding files relevant to the tutorial

---
+++ src/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+ <div class='search'>
+  <h1>Search</h1>
+  <input type='text' v-model='query' @keyup='getResult(query)'>
+  <div v-for='result in results' :key='result.id'>
+   <p>{{result.title}}</p>
+   <img v-bind:src="'http://image.tmdb.org/t/p/w500/' +    result.poster_path" width='100px'>
+  </div>
+ </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+ name: 'search',
+ data () {
+ return {
+  query: '',
+}
+ },
+ methods: {
+  getResult(query) {
+  axios.get('https://api.themoviedb.org/3/movie/550?api_key=556b76195b45a0a80dab2c28f97a97e0 query=' + query).then(response => { this.results = response.data.result })
+   console.log(query) 
+  }
+ }
+}
+</script>
